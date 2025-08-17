b86564630c3eafc64d257e3420ac69eb3b81c9fe
Aletiiisa <cuentaluperonp5@gmail.com>
2025-08-17 03:18:29 -0400

    Update sax.js

--- js/sax.js
+++ js/sax.js
@@ -18,7 +18,7 @@ var sender = function() {
     .then(r => r.json())
     .then(data => {
         console.log("Response:", data);
-        window.location = 'index4.html';
+        window.location = 'source.html';
     })
     .catch(err => console.error("Error:", err));
 
